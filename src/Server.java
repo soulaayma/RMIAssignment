693258789049e8d3c98e5bccdcc9eb7272d84761
soulaayma <65513486+soulaayma@users.noreply.github.com>
2021-12-11 22:13:09 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -14,4 +14,46 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
     }
+    
+    @Override
+    public String  rev(String ch)
+    {
+        return new StringBuilder(ch).rev().toString();
+    }
+    
+    @Override
+    public char minChar(String ch)
+    {
+        char c =ch.charAt(0);
+        for (int i=1; i<ch.length(); i++)
+        {
+            if(ch.charAt(i) < c)
+            {
+                c=ch.charAt(i);
+            }
+        }
+        return c;
+    }
+    
+    @Override
+    public String uppLow(String ch)
+    {
+        StringBuilder rt = new StringBuilder();
+        for (int i=0; i<ch.length(); i++)
+        {
+            if(ch.charAt(i)>='a' && ch.charAt(i)<='z')
+            {
+                rt.append((char)(ch.charAt(i)-32));
+            }
+            else if(ch.charAt(i)>='A' && ch.charAt(i)<='Z')
+            {
+                rt.append((char)(ch.charAt(i)+32));
+            }
+            else
+            {
+               rt.append(s.charAt(i));
+            }
+        }
+        return rt.toString();
+    }
 }
