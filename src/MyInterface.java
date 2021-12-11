0c911ade00ec03dc0c2baedd6a37c269731adac6
soulaayma <65513486+soulaayma@users.noreply.github.com>
2021-12-11 22:09:48 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
+  public String rev(String ch) throws RemoteException;
+    public char minChar(String ch) throws RemoteException;
+    public String uppLow(String ch) throws RemoteException;
 
 }
